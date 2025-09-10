71d12d8df0c6e67103671e9bff4a79b55271710e
Amit-4186 <amitkrpatel4.186@gmail.com>
2025-09-10 16:00:49 +0530

    frontend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'dotenv/config';
+import cors from 'cors';
 import shorturlsRouter from './routes/shortUrlRoute.js';
 import { handleRedirect } from './controllers/shortUrlController.js';
 import errorHandler from './middleware/errorHandler.js';
@@ -8,6 +9,7 @@ import { Log } from './utils/logger.js';
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/health', (req, res) => {
